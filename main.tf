54889f961336c1170b247580f7cf6803514ae356
Juhi K <jkesarwani.us@gmail.com>
2023-06-08 22:09:50 -0400

    Apps

--- main.tf
+++ main.tf
@@ -34,7 +34,7 @@ resource "aws_instance" "web" {
   instance_type = var.instance_type
   subnet_id     = var.subnets[0]
   vpc_security_group_ids = [aws_security_group.main.id]
-  tags          = merge(var.tags, { Name = "${var.name}-${var.env}-rabbitmq" })
+  tags          = merge(var.tags, { Name = "${var.name}-${var.env}" })
 
   root_block_device {
     encrypted = true
